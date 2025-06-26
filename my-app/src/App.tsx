e27ee04ef08b714d42c3d8504157938e7dfa8d00
eibednejo <abednegosantoso@gmail.com>
2025-06-26 18:04:22 +0700

    Update App.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'; // Changed to HashRouter
+import { HashRouter as Router, Route, Switch, Redirect, useLocation } from 'react-router-dom'; // Changed to HashRouter
 import LoginPage from './components/LoginPage';
 import SignupPage from './components/SignupPage';
 import AdminPage from './components/AdminPage';
@@ -26,9 +26,15 @@ const PrivateRoute = ({ component: Component, ...rest }: any) => (
     />
 );
 
+const DebugRoute = () => {
+    const location = useLocation();
+    return <div style={{position: 'fixed', top: 0, left: 0, background: '#ffc', zIndex: 9999}}>Current route: {location.pathname}</div>;
+};
+
 const App = () => {
     return (
         <Router>
+            <DebugRoute />
             <Switch>
                 <Route path="/login" component={LoginPage} />
                 <Route path="/signup" component={SignupPage} />
